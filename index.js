c544cb4fbbc3df53fc134d82448cfd259f98a81b
ViiCall <126810018+ViiCall@users.noreply.github.com>
2023-11-16 17:22:13 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+  let divlogin = document.querySelector('.container_login');
+  let divdash = document.querySelector('.conteiner__dashboard');
+  function sair(){
+    divdash.style.display ='none';
+    divdash.style.display= 'felx';
+  }
+function logar(){
+  divlogin.style.display = 'none';
+  divdash.style.display = 'flex';
+}
